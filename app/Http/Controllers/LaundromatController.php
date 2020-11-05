ea4cec54980b2cd5433cbbb64cac53c6ebbe8952
Dexalt142 <faturjackson14@gmail.com>
2020-11-05 23:06:49 +0700

    API laundromat endpoint

--- app/Http/Controllers/LaundromatController.php
+++ app/Http/Controllers/LaundromatController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Laundromat;
 use Illuminate\Http\Request;
 
 class LaundromatController extends Controller
 {
-    //
+    
+    public function getLaundromats() {
+        $laundromats = Laundromat::all();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Fetch success',
+            'data' => $laundromats
+        ]);
+    }
+
 }
