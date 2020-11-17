1501412d798463a84d3a1026d3083e81d369a035
Dexalt142 <faturjackson14@gmail.com>
2020-11-17 10:35:23 +0700

    User model jwt subject & roles

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -16,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role',
     ];
 
     /**
@@ -37,11 +38,33 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
     public function laundromat() {
         return $this->hasOne(\App\Laundromat::class);
     }
 
     public function transactions() {
         return $this->hasMany(\App\Transaction::class);
+    }
+
+    public function isLaundromatOwner() {
+        return ($this->role == 1);
     }
 }
